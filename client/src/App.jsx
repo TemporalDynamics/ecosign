cab4094c25e3ff610095e98a4744fdada5fbd14b
manu <temporaldynamicsllc@protonmail.com>
2025-12-12 02:55:22 -0600

    feat: add News section with articles and newsletter signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -54,6 +54,7 @@ const SignWorkflowPage = lazy(() => import('./pages/SignWorkflowPage'));
 const WorkflowsPage = lazy(() => import('./pages/WorkflowsPage'));
 const WorkflowDetailPage = lazy(() => import('./pages/WorkflowDetailPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage'));
+const NewsArticlePage = lazy(() => import('./pages/NewsArticlePage'));
 
 
 function AppRoutes() {
@@ -93,6 +94,7 @@ function AppRoutes() {
             <Route path="/report" element={<ReportPage />} />
             <Route path="/report-issue" element={<ReportIssuePage />} />
             <Route path="/news" element={<NewsPage />} />
+            <Route path="/news/:slug" element={<NewsArticlePage />} />
             
             {/* Resources routes */}
             <Route path="/documentation" element={<DocumentationPage />} />
